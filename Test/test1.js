8565c4f56e13e99268a26b32f75f00262e6aa50b
Thaolinh0209 <Thaolinhdn99@gmail.com>
2020-08-21 23:02:01 +0700

    add

--- Test/test1.js
+++ Test/test1.js
@@ -1,8 +1,11 @@
 var a = 20;
 var b = 5;
-var c = 17;
+var c = 50;
 var arr = [a,b,c];
-
-console.log(arr.sort(function(a,b){
-    return a-b;
-}));
+var max = arr[0];
+for(var i = 1; i<arr.length; i++){
+    if(max < arr[i]){
+        max = arr[i];
+    }
+};
+console.log(max);
