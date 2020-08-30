df48275ed9991ab891a6044e1c1301b337f437c7
Thaolinh0209 <Thaolinhdn99@gmail.com>
2020-08-30 14:10:13 +0700

    30/08

--- Test/test6.js
+++ Test/test6.js
@@ -1,5 +1,21 @@
-var arr = [0,1,2,3,4];
-var arr1 = [];
-var x = 0;
+var arr=[0,1,2,3,4,5];
+var arr1=[];
+var sum=0;
+//map
+// arr1 = arr.map((value,index,arr) => {
+//     return value;
+// })
+// console.log(arr1);
 
-console.log(arr.slice(0,3));
+//filer
+// arr1 = arr.filter((value,index,arr) => {
+//     return value > 1;
+// })
+// console.log(arr1);
+
+
+//include
+sum = arr.reduce((sum,value,index,arr) => {
+    return sum+=value;
+})
+console.log(sum);
